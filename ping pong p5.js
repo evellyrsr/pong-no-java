b7a6ecf298c5fe77d2ff58ca0260f2e894e2a0a4
Evellyn R <98847317+fvcku-wxyz@users.noreply.github.com>
2024-06-08 18:29:11 -0300

    Update ping pong p5.js
    adicionado a "minha raquete" e movimentação

--- ping pong p5.js
+++ ping pong p5.js
@@ -1,20 +1,26 @@
+// variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let diametroBolinha = 25
-let velocidadeXBolinha = 6;
-let velocidadeYBolinha = 6;
 let raioBolinha = diametroBolinha / 2;
 
+// velocidade da bolinha
+let velocidadeXBolinha = 2;
+let velocidadeYBolinha = 2;
+
+// desenho bolinha
 function mostraBolinha(){
    fill("white");
   circle(xBolinha, yBolinha, diametroBolinha); 
 }
 
+// movimentos da bolinha
 function movimentoBolinha(){
     xBolinha += velocidadeXBolinha;
   yBolinha += velocidadeYBolinha;
 }
 
+// colisão da bolinha com as bordas
   function colisãoBolinha(){
       if(xBolinha + raioBolinha > width ||
      xBolinha - raioBolinha < 0){
@@ -26,6 +32,27 @@ function movimentoBolinha(){
 }
   }
 
+// variáveis da raquete
+let xRaquete = 5
+let yRaquete = 150
+let comprimentoRaquete = 10
+let alturaRaquete = 90
+
+// desenho raquete
+function mostraRaquete(){
+  rect(xRaquete, yRaquete, comprimentoRaquete, alturaRaquete);
+}
+
+// movimento da minha raquete
+function movimentoRaqueteMy(){
+  if(keyIsDown(UP_ARROW)){ 
+  yRaquete -= 10;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+  yRaquete += 10;
+  }
+  
+}
 function setup() {
   createCanvas(600, 400);
 }
@@ -35,4 +62,7 @@ function draw() {
   mostraBolinha();
   movimentoBolinha();
   colisãoBolinha();
+  mostraRaquete();
+  movimentoRaqueteMy();
+  
 }
