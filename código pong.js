a9285562559d1f540110ac2be05b2b9491ee74d7
Evellyn R <98847317+fvcku-wxyz@users.noreply.github.com>
2024-06-09 15:05:13 -0300

    Update código pong.js
    sons adicionado ao jogo + jogo multiplayer
    jogador 1 (esquerda) = W + S
    jogador 2 (direita) = < + > (SETINHAS esquerda e direita)

--- código pong.js
+++ código pong.js
@@ -1,3 +1,6 @@
+// nota
+ console.log('nota: Há o mesmo bug (da bolinha quando colidi em uma certa parte da raquete), que acontecia no Scratch, acontecendo aqui. Ainda não sei o que é!');
+
 // BOLINHA
 
 // variáveis da bolinha
@@ -43,10 +46,10 @@ let alturaRaquete = 90
 
 // movimento da minha raquete
 function movimentoRaqueteMinha(){
-  if(keyIsDown(UP_ARROW)){ 
+  if(keyIsDown(87)){ 
     yRaquete -= 8;
   }
-  if(keyIsDown(DOWN_ARROW)){
+  if(keyIsDown(83)){
     yRaquete += 8;
 }}
 
@@ -56,35 +59,88 @@ let yRaqueteOponente = 150;
 
 // movimento da raquete oponente
 function movimentoRaqueteOponente(){
-  velocidadeYOponente = yBolinha -yRaqueteOponente - comprimentoRaquete / 2 - 30;
-  yRaqueteOponente += velocidadeYOponente;
+  if(keyIsDown(UP_ARROW)){ 
+    yRaqueteOponente -= 8;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    yRaqueteOponente += 8;
 }
-
+}
 // desenho raquetes
 function mostraRaquete(x, y){
   rect(x, y, comprimentoRaquete, alturaRaquete);
 }
 
 // COLISÃO
+
 let colisao = false
 
+// colisão bolinha com minha raquete
 function colisaoBolinhaRaquete(){
   if(xBolinha - raioBolinha < xRaquete + comprimentoRaquete
      && yBolinha - raioBolinha < yRaquete + alturaRaquete
      && yBolinha + raioBolinha > yRaquete){
     velocidadeXBolinha *= -1;
+    sRaquetada.play();
 }}
 
 function colisaoRaquetes(x, y){
  colisao =
    collideRectCircle(x, y, comprimentoRaquete, alturaRaquete, xBolinha, yBolinha, raioBolinha);
   if(colisao){
-    velocidadeXBolinha *= -1
+    velocidadeXBolinha *= -1;
+    sRaquetada.play();
 }}
+
+// SISTEMA DE PONTUAÇÃO
+let meusPontos = 0;
+let pontosOponente = 0;
+
+// pontuação na tela
+function mostrarPontos(){
+  
+//  stroke("white");
+//prefiro deixar sem a borda, pelo visual do game
+      fill("orange");
+  rect(430, 10, 40, 20);
+  rect(130, 10, 40, 20);
+
+  fill("white");
+  textAlign(CENTER);
+  textSize(16);
+  text(meusPontos, 150, 26);
+  text(pontosOponente, 450, 26);
+}
+
+// atribuição de pontos
+function atribuirPontos(){
+  if(xBolinha > 585){
+    meusPontos += 1;
+    sPonto.play();
+  }
+  
+  if(xBolinha < 15){
+    pontosOponente += 1;
+    sPonto.play();
+  }
+}
+
+// SONS DO JOGO
+let sRaquetada;
+let sTrilhaSonora;
+let sPonto;
+
+function preload(){
+  sRaquetada = loadSound("raquetada.mp3")
+  sTrilhaSonora = loadSound("trilha.wav")
+  sPonto = loadSound("ponto.mp3")
+}
 
 // SETUP JOGO
 function setup() {
   createCanvas(600, 400);
+  sTrilhaSonora.loop();
+
 }
 
 function draw() {
@@ -99,4 +155,6 @@ function draw() {
   colisaoBolinhaRaquete();
   colisaoRaquetes(xRaquete, yRaquete);
   colisaoRaquetes(xRaqueteOponente, yRaqueteOponente);
+  mostrarPontos();
+  atribuirPontos();
 }
